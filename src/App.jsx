1ce0160f8e1f7ebbce8a6f5dee089600a7171f75
Mosayeb09 <mosayebhossain09@gmail.com>
2024-04-22 19:02:03 +0600

    complet nav and hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 
+import Hero from './assets/Component/Hero/Hero';
 import Navbar from './assets/Component/Navbar/Navbar';
+import Program from './assets/Component/Programs/Program';
+import Title from './assets/Component/Title/Title';
 
 const App = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <Hero></Hero>
+      <div className="container">
+        <Title></Title>
+          <Program></Program>
+      </div>
+      
     </div>
   );
 };
---
+++ src/assets/Component/Title/Title.jsx
@@ -0,0 +1,13 @@
+import './Title.css'
+
+const Title = () => {
+    return (
+        <div className='title'>
+            <p>Our PROGRAM</p>
+            <h2>What we offer</h2>
+            
+        </div>
+    );
+};
+
+export default Title;
