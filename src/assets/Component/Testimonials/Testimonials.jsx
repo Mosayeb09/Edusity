f265cfa72d12b9d0bcf8791e296077515df03b42
Mosayeb09 <mosayebhossain09@gmail.com>
2024-04-23 13:46:41 +0600

    adding cart to the Testimonials section

--- src/assets/Component/Testimonials/Testimonials.jsx
+++ src/assets/Component/Testimonials/Testimonials.jsx
@@ -12,6 +12,58 @@ const Testimonials = () => {
         <div className='testimonials'>
             <img src={Next_icon} alt="" className='next-btn' />
             <img src={Back_icon} alt="" className='back-btn' />
+            <div className="slider">
+                <ul>
+                    <li>
+                        <div className="slide">
+                            <div className="user-info">
+                                <img src={user_1} alt="" />
+                                <div>
+                                    <h3>Emily Williams</h3>
+                                    <span>Edusity,USA</span>
+                                </div>
+                            </div>
+                            <p>Choosing to pursue my degree at Edusity was one of the best decisions I've ever made. The supportive community, state-of-the-art facilities, and commitment to academic excellence have truly exceeded my expectations.</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="slide">
+                            <div className="user-info">
+                                <img src={user_2} alt="" />
+                                <div>
+                                    <h3>William Jackson</h3>
+                                    <span>Edusity,USA</span>
+                                </div>
+                            </div>
+                            <p>Choosing to pursue my degree at Edusity was one of the best decisions I've ever made. The supportive community, state-of-the-art facilities, and commitment to academic excellence have truly exceeded my expectations.</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="slide">
+                            <div className="user-info">
+                                <img src={user_3} alt="" />
+                                <div>
+                                    <h3>Emily Williams</h3>
+                                    <span>Edusity,USA</span>
+                                </div>
+                            </div>
+                            <p>Choosing to pursue my degree at Edusity was one of the best decisions I've ever made. The supportive community, state-of-the-art facilities, and commitment to academic excellence have truly exceeded my expectations.</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="slide">
+                            <div className="user-info">
+                                <img src={user_4} alt="" />
+                                <div>
+                                    <h3>William Jackson</h3>
+                                    <span>Edusity,USA</span>
+                                </div>
+                            </div>
+                            <p>Choosing to pursue my degree at Edusity was one of the best decisions I've ever made. The supportive community, state-of-the-art facilities, and commitment to academic excellence have truly exceeded my expectations.</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
             
         </div>
     );
